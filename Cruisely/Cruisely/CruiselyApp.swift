7097c59a351db73a7284027827ddfdf61f36dd5b
james.mcdougall <jameswilliammcdougall@gmail.com>
2023-03-04 09:18:24 -0600

    Initial Project Commit

---
+++ Cruisely/Cruisely/CruiselyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CruiselyApp.swift
+//  Cruisely
+//
+//  Created by James McDougall on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CruiselyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
